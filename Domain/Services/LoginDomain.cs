e00b7fe2f34ba34380f5a39f2cfd6174e6cfeb28
João Chagas <jpchagas@tasken.com>
2021-12-26 13:29:56 -0300

    feat: adiciona login e criação de usuario

---
+++ Domain/Services/LoginDomain.cs
@@ -0,0 +1,34 @@
+﻿using Infrastructure.DTO;
+using Infrastructure.Interfaces;
+using Infrastructure.Models;
+using System;
+
+namespace Domain.Services
+{
+    public class LoginDomain
+    {
+        private readonly IUserProvider _userProvider;
+        private readonly IHashProvider _hashProvider;
+
+        public LoginDomain(IUserProvider userProvider, IHashProvider hashProvider)
+        {
+            _userProvider = userProvider;
+            _hashProvider = hashProvider;
+        }
+
+        public User Login(LoginRequest request)
+        {
+            User user = _userProvider.GetByEmail(request.Email);
+
+            if (user == null)
+                throw new Exception("User and/or password is invalid");
+
+            string password = _hashProvider.GenerateHash(request.Password);
+
+            if (user.Password != password)
+                throw new Exception("User and/or password is invalid");
+
+            return user;
+        }
+    }
+}
---
+++ Domain/Services/UserDomain.cs
@@ -0,0 +1,48 @@
+﻿using Infrastructure.DTO;
+using Infrastructure.Interfaces;
+using Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Domain.Services
+{
+    public class UserDomain
+    {
+        private readonly IUserProvider _userProvider;
+        private readonly IHashProvider _hashProvider;
+
+        public UserDomain(IUserProvider userProvider, IHashProvider hashProvider)
+        {
+            _userProvider = userProvider;
+            _hashProvider = hashProvider;
+        }
+
+        public List<User> GetUser()
+        {
+            return _userProvider.GetUsers();
+        }
+
+        public User CreateUser(UserRequest request)
+        {
+            var dateTimeNow = DateTime.Now.ToUniversalTime();
+            string password = _hashProvider.GenerateHash(request.Password);
+
+            User user = new()
+            {
+                Name = request.Name,
+                Email = request.Email,
+                Password = password,
+                Created = dateTimeNow,
+                LastLogin = dateTimeNow,
+                Modified = dateTimeNow,
+            };
+
+            var response = _userProvider.CreateUser(user);
+
+            if(user == null)
+                throw new Exception("This user already exists");
+
+            return response;            
+        }
+    }
+}
